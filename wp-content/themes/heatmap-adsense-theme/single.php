99702eb95c3c6e347271411115e20e77d49297cf
jluckett <jluckett@ourstage.com>
2011-01-04 10:53:11 -0500

    same as last commit

--- wp-content/themes/heatmap-adsense-theme/single.php
+++ wp-content/themes/heatmap-adsense-theme/single.php
@@ -63,7 +63,7 @@ Widget Area: [Content Item] Above
 <!--
 Widget Area: [Content Item] Left 
 ~~~ -->	
-			<?php   dynamic_sidebar('[Content Item] Left'); ?>
+			<?php if(is_single()) : dynamic_sidebar('[All Content] Above'); ?>
 <!--
 Widget Area: [Content Item] Right 
 ~~~ -->
