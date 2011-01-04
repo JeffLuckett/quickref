6fc000202ed6e8237d7c79d8a082890306c55209
jluckett <jluckett@ourstage.com>
2011-01-04 10:51:02 -0500

    Remove widget zone from multi-post listings

--- wp-content/themes/heatmap-adsense-theme/page.php
+++ wp-content/themes/heatmap-adsense-theme/page.php
@@ -48,7 +48,7 @@ The Post
 <!--
 Widget Area: [All Content] Above
 ~~~ -->
-			<?php dynamic_sidebar('[All Content] Above'); ?>
+			<?php if(is_single()) : dynamic_sidebar('[All Content] Above'); ?>
 	
 <!--
 The Post Title 
